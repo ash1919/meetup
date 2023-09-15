f8f989ee0e8124cd97f1e70d792536b4c1394c73
ash1919 <akashjayaraj87@gmail.com>
2023-09-15 13:29:27 +0530

    fix: logo
    Signed-off-by: ash1919 <akashjayaraj87@gmail.com>

--- src/component/meetup-landingpage/footer.js
+++ src/component/meetup-landingpage/footer.js
@@ -13,41 +13,27 @@ export const Footer = () => {
             : "flex flex-col md:flex-row items-center space-x-8 text-white w-full sm:container p-6 mt-24 md:mb-20 mb-28 sm:mb-28 xl:mb-20"
         }
       >
-        <div>
-          <img
-            src="../images/ineuronLogo.png"
-            alt="ineuron"
-            className="pl-4 md:pl-0"
-          />
-        </div>
+        <a href="/">
+          <img src="../images/logo.png" alt="meetup" />
+        </a>
         <div className="flex flex-col md:flex-row pt-6 text-center md:pt-0 text-white text-xs xl:text-sm items-center justify-center md:justify-between w-full">
           <div className="pb-2 md:pb-0">
             <p className="sm:text-center sm:text-start md:text-left">
-              <a href="https://ineuron.ai/terms.html" target="_blank">
-                Terms of Use
-              </a>
-
-              <a href="https://ineuron.ai/policy.html" target="_blank">
-                {" "}
-                | Privacy Policy
-              </a>
+              Terms of Use | Privacy Policy
             </p>
             <p className="pt-1 text-darkgrey sm:px-48 md:px-0">
-              © iNeuron Intelligence Pvt. Ltd. 2022. All Rights Reserved.
+              © Meetup Pvt. Ltd. 2022. All Rights Reserved.
             </p>
           </div>
           <div className="flex space-x-6 h-4 pt-4 md:pt-0 pb-6 md:pb-0">
-            <a
-              href="https://www.youtube.com/channel/UCb1GdqUqArXMQ3RS86lqqOw"
-              target="_blank"
-            >
+            <a href="/" target="_blank">
               <img
                 src="../images/youtube-nav.png"
                 alt="linkdin"
                 className="w-6"
               />
             </a>
-            <a href="https://twitter.com/iNeuronAi" target="_blank">
+            <a href="/" target="_blank">
               <img
                 src="../images/twitter-nav.png"
                 alt="you-tube"
@@ -55,7 +41,7 @@ export const Footer = () => {
               />
             </a>
             <a
-              href="https://www.linkedin.com/company/ineuron-ai/"
+             href="/"
               target="_blank"
             >
               <img
@@ -64,7 +50,7 @@ export const Footer = () => {
                 className="w-4"
               />
             </a>
-            <a href="https://www.facebook.com/ineuronai" target="_blank">
+            <a href="/" target="_blank">
               <img src="../images/fb-nav.png" alt="twitter" className="w-2" />
             </a>
           </div>
--- src/component/meetup-landingpage/navbar.js
+++ src/component/meetup-landingpage/navbar.js
@@ -7,31 +7,10 @@ function Navbar() {
         <div className="sm:container container  flex items-center space-x-12">
           <div>
             <figure>
-              <img src="../images/ineuronLogo.png" alt="Ineuron" />
+              <a href="/">
+                <img src="../images/logo.png" alt="meetup" />
+              </a>
             </figure>
-          </div>
-          <div className="hidden md:flex space-x-14 ">
-            <a
-              href="https://courses.ineuron.ai/"
-              target="_blank"
-              className="hover:text-green"
-            >
-              Course library
-            </a>
-            <a
-              href="https://one-neuron.ineuron.ai/"
-              target="_blank"
-              className="hover:text-green"
-            >
-              One Neuron
-            </a>
-            <a
-              href="https://internship.ineuron.ai/"
-              target="_blank"
-              className="hover:text-green"
-            >
-              Internship
-            </a>
           </div>
         </div>
       </nav>
